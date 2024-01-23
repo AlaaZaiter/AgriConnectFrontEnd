da27c3a125f64c573bbcbc2d9ce0a67faf77e6cd
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-23 21:02:08 +0200

    setting domain server

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: [
-      'https://agri-connect-platform-sgr1.onrender.com',
+      'agri-connect-platform-sgr1.onrender.com',
     ],
-
   },
-})
+});
