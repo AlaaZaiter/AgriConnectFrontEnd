9cc2c08bfdeab3edea98f7a93055b0ae96280c46
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-23 21:42:33 +0200

    setting domain server

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: [
-      'https://backendagri.onrender.com',
-    ],
+    disableHostCheck: true,
+
   },
 });
