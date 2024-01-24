f934e07e0ff6a7f852e0b5993e626f4e5fa56cf1
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 12:04:19 +0200

    solving webpack problem

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,10 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    allowedHosts: [
+      'agri-connect-platform-sgr1.onrender.com',
+    ],
     
-    public: 'agri-connect-platform-sgr1.onrender.com:8080',
-
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 
