733ad1abd8883445ae4636effd5090ede20d19ba
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 13:22:20 +0200

    solving webpack problem

--- vue.config.js
+++ vue.config.js
@@ -10,26 +10,25 @@ module.exports = defineConfig({
     ],
   },
   
-  configureWebpack: {
-    publicPath: process.env.NODE_ENV === 'production' ? '/agriconnect/' : '/',
-    plugins: [
-      {
-        apply: (compiler) => {
-          compiler.hooks.compilation.tap('CustomHtmlPlugin', (compilation) => {
-            if (compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
-              compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('CustomHtmlPlugin', (data, callback) => {
-                // You can customize the title here
-                data.htmlWebpackPlugin.options.title = 'agriconnect';
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'agriconnect';
+      return args;
+    });
 
-                // Exclude certain chunks from the HTML file
-                data.assets.js = data.assets.js.filter((path) => !path.includes('main.js'));
+    config.plugin('CustomHtmlPlugin').use({
+      apply: (compiler) => {
+        compiler.hooks.compilation.tap('CustomHtmlPlugin', (compilation) => {
+          if (compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
+            compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('CustomHtmlPlugin', (data, callback) => {
+              // Exclude certain chunks from the HTML file
+              data.assets.js = data.assets.js.filter((path) => !path.includes('main.js'));
 
-                callback(null, data);
-              });
-            }
-          });
-        },
+              callback(null, data);
+            });
+          }
+        });
       },
-    ],
+    });
   },
 });
