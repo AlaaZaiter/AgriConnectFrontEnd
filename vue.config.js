20e99544a6680dcf25f130920e1bdd1f7f83b028
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 12:50:42 +0200

    solving webpack problem

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: [
-      'onrender.com',
+      'agri-connect-platform-sgr1.onrender.com',
     ],
     
   },
