b550de70df731026eb0c2607f18edbd0236c4597
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 10:50:50 +0200

    solving webpack problem

--- vue.config.js
+++ vue.config.js
@@ -7,4 +7,20 @@ module.exports = defineConfig({
       'agri-connect-platform-sgr1.onrender.com',
     ],
   },
+  configureWebpack: (config) => {
+    config.plugins.push({
+      apply: (compiler) => {
+        compiler.hooks.compilation.tap('CustomHtmlPlugin', (compilation) => {
+          compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('CustomHtmlPlugin', (data, callback) => {
+            // Exclude certain chunks from the HTML file
+            data.assets.js = data.assets.js.filter((path) => !path.includes('main.js'));
+
+            // You can customize this logic based on your needs
+
+            callback(null, data);
+          });
+        });
+      },
+    });
+  },
 });
