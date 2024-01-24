7d1c82382796e506012002e8d5aeff19b59fce12
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 21:59:49 +0200

    check deplymnet conditions

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   outputDir: 'dist', // Adjust the path accordingly
+  devServer: {
+    allowedHosts: [
+      'agri-connect-platform-sgr1.onrender.com',
+      // Add more allowed hosts if needed
+    ],
+    port: 8080, // You can change the port number if needed
+  },
   configureWebpack: {
     entry: './src/main.js',
   },
