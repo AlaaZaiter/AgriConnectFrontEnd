e0be477fafb24202a5d6821036f15d13d8654842
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 11:13:19 +0200

    solving webpack problem

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+// vue.config.js
+
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
@@ -12,13 +14,13 @@ module.exports = defineConfig({
       {
         apply: (compiler) => {
           compiler.hooks.compilation.tap('CustomHtmlPlugin', (compilation) => {
-            // Ensure the HtmlWebpackPlugin is available before using its hooks
             if (compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
               compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('CustomHtmlPlugin', (data, callback) => {
+                // You can customize the title here
+                data.htmlWebpackPlugin.options.title = 'agriconnect';
+
                 // Exclude certain chunks from the HTML file
                 data.assets.js = data.assets.js.filter((path) => !path.includes('main.js'));
-
-                // You can customize this logic based on your needs
 
                 callback(null, data);
               });
