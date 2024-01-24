705a1f56875e3b931e62b2b77afe27b4b7cdb67d
AlaaZaiter2 <zaiteralaa29@gmail.com>
2024-01-24 21:34:46 +0200

    check deplymnet conditions

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: ['vue'], // Example: Add specific dependencies to transpile
-  devServer: {
-    // Configuration for the development server
-    // ...
-  },
   configureWebpack: {
-    plugins: [
-      {
-        apply: (compiler) => {
-          compiler.hooks.compilation.tap('CustomHtmlPlugin', (compilation) => {
-            if (compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) {
-              compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('CustomHtmlPlugin', (data, callback) => {
-                // You can customize the title here
-                data.htmlWebpackPlugin.options.title = 'agriconnect';
-
-                // Exclude certain chunks from the HTML file
-                data.assets.js = data.assets.js.filter((path) => !path.includes('main.js'));
-
-                callback(null, data);
-              });
-            }
-          });
-        },
-      },
-    ],
     entry: './src/main.js',
     outputDir: 'dist', // Adjust the path accordingly
   },
